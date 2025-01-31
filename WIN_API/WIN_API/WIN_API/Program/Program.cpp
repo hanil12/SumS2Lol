e3bd1330d1fee4af122ed734e260b92d9df30532
hanil12 <ekdrhaod@gmail.com>
2025-01-31 11:48:20 +0900

    BackBuffer

--- WIN_API/WIN_API/WIN_API/Program/Program.cpp
+++ WIN_API/WIN_API/WIN_API/Program/Program.cpp
@@ -7,13 +7,22 @@
 #include "Scene/LineCollisionScene.h"
 #include "Scene/CannonScene.h"
 
+HDC Program::backbuffer = nullptr;
 Program::Program()
 {
-	_scene = make_shared<CollisionScene>();
+	_scene = make_shared<CannonScene>();
+
+	HDC hdc = GetDC(hWnd);
+
+	backbuffer = CreateCompatibleDC(hdc);
+	_hBitMap = CreateCompatibleBitmap(hdc, WIN_WIDTH, WIN_HEIGHT); // 그림을 그릴 도화지
+	SelectObject(backbuffer, _hBitMap);
 }
 
 Program::~Program()
 {
+	DeleteObject(backbuffer);
+	DeleteObject(_hBitMap);
 }
 
 void Program::Update()
@@ -23,5 +32,11 @@ void Program::Update()
 
 void Program::Render(HDC hdc)
 {
-	_scene->Render(hdc);
+	PatBlt(backbuffer, 0, 0, WIN_WIDTH, WIN_HEIGHT, BLACKNESS);
+
+	_scene->Render(backbuffer);
+
+	// 복사
+	BitBlt(hdc, 0, 0, WIN_WIDTH, WIN_HEIGHT,
+		backbuffer, 0, 0, SRCCOPY);
 }
