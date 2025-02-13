4c1c7a6e873125e025857d1b35bbba2d0b86aaea
hanil12 <ekdrhaod@gmail.com>
2025-02-13 12:47:16 +0900

    Map VS uMap

--- WIN_API/WIN_API/Algorithm/BinarySearchTree.cpp
+++ WIN_API/WIN_API/Algorithm/BinarySearchTree.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 #include <stack>
 #include <queue>
+#include <map>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
@@ -20,6 +21,34 @@ using namespace std;
 // - (B-트리 => DB)
 // - AVL
 
+//           이진탐색트리    vs     해쉬테이블 (표)
+//            Node(key,data)       pair(key, data) 
+//           key로 정렬             해쉬(key...Algorithm) => Index
+// 탐색시간    logN                  1
+// 삽입시간    logN                  1
+// 삭제        logN                  1
+
+// => 이진탐색트리를 쓰는 이유
+// 1. 데이터를 많이 쓴다. (데이터 파편화가 되어있으면 해시테이블을 쓰기 힘들 수 있다.)
+// 2. 해쉬함수의 성능이 떨어질 경우 '체이닝' 발생
+
+class Player
+{
+public:
+	bool operator<(const Player& other) const
+	{
+		return hp < other.hp;
+	}
+
+	bool operator==(const Player& other) const
+	{
+		return hp == other.hp;
+	}
+
+	int hp;
+	int atk;
+};
+
 int main()
 {
 	BinarySearchTree tree;
@@ -34,6 +63,19 @@ int main()
 
 	tree.PrintTree(tree.root);
 
+	map<string, int> m;
+
+	if(m.count("Hanil1") == 0)
+		m["Hanil1"] = 1;	
+	auto iter = m.insert(make_pair<string, int>("Hanil2", 2));
+
+	for (auto p : m)
+	{
+		cout << p.first << "... " << p.second << endl;
+	}
+
+	map<Player, int> pm;
+	pm[{10, 5}] = 5;
 
 	return 0;
 }
