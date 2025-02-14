2459579c6ad979fc2e1295cd6c3e23d56b14823f
hanil12 <ekdrhaod@gmail.com>
2025-02-14 13:25:54 +0900

    DP

--- WIN_API/WIN_API/Algorithm/BST.h
+++ WIN_API/WIN_API/Algorithm/BST.h
@@ -24,17 +24,18 @@ public:
 	~BinarySearchTree();
 
 	void Insert(int key);
-	void PrintTree(Node* node);
+	void PrintTree(Node* root);
 
-	Node* Search(Node* node, int key); // key값을 가지는 노드 반환
-	Node* Min(Node* node); // node부터 시작하는 트리에서 가장작은값을 가지는 노드 반환
-	Node* Max(Node* node); //   "                      가장큰값    "
+	Node* Search(Node* root, int key); // key값을 가지는 노드 반환
+	Node* Min(Node* root); // node부터 시작하는 트리에서 가장작은값을 가지는 노드 반환
+	Node* Max(Node* root); //   "                      가장큰값    "
 
-	Node* Previous(Node* node); // 매개변수 node보다 한칸 더 작은 얘
-	Node* Next(Node* node); // 매개변수 node보다 한칸 더 큰 얘
-
-	void Replace(Node* node1, Node* node2);
 	void Delete(Node* node);
 
 	Node* root = nullptr;
+private:
+	Node* Previous(Node* root); // 매개변수 root보다 한칸 더 작은 얘
+	Node* Next(Node* root); // 매개변수 root보다 한칸 더 큰 얘
+
+	void Replace(Node* node1, Node* node2);
 };
