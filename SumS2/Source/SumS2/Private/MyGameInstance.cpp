0c6c8621c91bf12fcae4c82028b0b9cb4aeea2e7
hanil12 <ekdrhaod@gmail.com>
2025-02-28 10:04:23 +0900

    StatTable

---
+++ SumS2/Source/SumS2/Private/MyGameInstance.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+#include "MyStatComponent.h"
+#include "Engine/DataTable.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	static ConstructorHelpers::FObjectFinder<UDataTable> dataTable(TEXT("/Script/Engine.DataTable'/Game/Data/MyStatTable.MyStatTable'"));
+
+	if (dataTable.Succeeded())
+	{
+		_statTable = dataTable.Object;
+	}
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+	FString rowName = "Level_3";
+	auto row = _statTable->FindRow<FMyStatData>(*rowName, TEXT(""));
+
+	UE_LOG(LogTemp, Error, TEXT("Level : %d, Hp : %d, ATK : %d"), row->level, row->hp, row->atk);
+}
