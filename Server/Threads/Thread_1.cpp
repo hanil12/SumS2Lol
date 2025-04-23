3919320c51a661ac8935e51481f89173c5edc23b
hanil12 <ekdrhaod@gmail.com>
2025-04-23 17:40:01 +0900

    Atomic, Mutex

---
+++ Server/Threads/Thread_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <thread>
+
+using namespace std;
+
+void HelloThread()
+{
+	cout << "Hello Thread!" << endl;
+}
+
+int main()
+{
+	std::thread t(&HelloThread);
+
+	t.join();
+	// join : t가 끝날 때까지 대기
+	// joinable : join 가능한지
+	// get_id : 쓰레드 아이디
+	// detach : 쓰레드 분리
+	// hardware_concurrency : 현재 운용가능한 쓰레드의 개수
+
+	return 0;
+}
---
+++ Server/Threads/Thread_3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+using namespace std;
+
+vector<int> v;
+std::mutex m;
+
+// lock
+// mutex : mutual exclusion
+
+// RAII : Resource Acquisition is Initilization 
+// 리소스 획득은 초기화, 리소스 드랍은 소멸
+
+template<typename T>
+class LockGuard
+{
+public:
+	LockGuard(T* mutex) : _mutex(mutex) { _mutex->lock(); } // mutex 획드
+	~LockGuard() { _mutex->unlock(); }
+
+private:
+	T* _mutex;
+};
+
+void Push(int num)
+{
+	LockGuard<std::mutex> lg(&m);
+
+	v.push_back(num);
+}
+
+int main()
+{
+	std::thread t1([]()-> void
+	{
+			for (int i = 0; i < 1000; i++)
+			{
+				Push(i);
+			}
+	});
+
+	std::thread t2([]()-> void
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				Push(i);
+			}
+		});
+
+	t1.join();
+	t2.join();
+
+	cout << v.size() << endl;
+
+	return 0;
+}
