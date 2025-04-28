7d44bd84c7cb83dbb66dcf1fb5f5846aa06d3c53
hanil12 <ekdrhaod@gmail.com>
2025-04-28 18:46:24 +0900

    Exercise

--- Server/Threads/Exercise.cpp
+++ Server/Threads/Exercise.cpp
@@ -8,15 +8,7 @@
 
 using namespace std;
 
-void PrimeCount_One()
-{
-
-}
-
-void PrimeCount_Two()
-{
-
-}
+int MAX_C = 1000000;
 
 bool IsPrime(int num)
 {
@@ -37,16 +29,31 @@ bool IsPrime(int num)
 	return true;
 }
 
+
 int main()
 {
 	__int64 start = GetTickCount64();
 
+	int OneCount = 0;
+	for (int i = 0; i < MAX_C; i++)
+	{
+		if (IsPrime(i))
+		{
+			OneCount++;
+		}
+	}
+
+	__int64 end = GetTickCount64();
+	cout << OneCount << endl;
+	cout << end - start << "ms" << endl;
+
+	start = GetTickCount64();
 	// 작업
 	atomic<int> primeCount = 0;
 
 	vector<std::thread> threads;
 	int threadCount = thread::hardware_concurrency();
-	int jobCount = (100000 / threadCount); // 100000
+	int jobCount = (MAX_C / threadCount); // 100000
 
 	for (int i = 0; i < threadCount; i++)
 	{
@@ -55,7 +62,7 @@ int main()
 		// thread2 : 10000 ~ 20000
 		// ...
 		int start = (i * jobCount);
-		int end = min(100000,(i + 1) * jobCount);
+		int end = min(MAX_C,(i + 1) * jobCount);
 
 		threads.push_back(thread(([start, end, &primeCount]()->void
 			{
@@ -70,7 +77,7 @@ int main()
 	for(auto& t : threads)
 		t.join();
 
-	__int64 end = GetTickCount64();
+	end = GetTickCount64();
 
 	__int64 temp = end -start;
 	cout << primeCount << endl;
