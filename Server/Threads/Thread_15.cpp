40858786c47fc895442d95089a223341bb93a1c9
hanil12 <ekdrhaod@gmail.com>
2025-05-09 18:13:46 +0900

    RW_DONE

--- Server/Threads/Thread_15.cpp
+++ Server/Threads/Thread_15.cpp
@@ -1,10 +1,74 @@
 #include "framework.h"
 
+class TestJob
+{
+	USE_LOCK
+
+public:
+	int TestRead()
+	{
+		READ_LOCK
+		if(q.empty() == false)
+			return q.front();
+
+		return -1;
+	}
+
+	void TestPush()
+	{
+		WRITE_LOCK
+
+		q.push(rand() % 100);
+	}
+
+	void TestPop()
+	{
+		WRITE_LOCK
+
+		if(q.empty() == false)
+			q.pop();
+	}
+
+private:
+	queue<int> q;
+};
+
+TestJob GJob;
+
+void ThreadRead()
+{
+	while (true)
+	{
+		int num = GJob.TestRead();
+		cout << num << endl;
+	}
+}
+
+void ThreadWrite()
+{
+	while (true)
+	{
+		GJob.TestPush();
+		this_thread::sleep_for(3000ms);
+		GJob.TestPop();
+	}
+}
+
 int main()
 {
 	ThreadManager::Create();
 
-	
+	for (int i = 0; i < 2; i++)
+	{
+		TM->Launch(ThreadWrite);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		TM->Launch(ThreadRead);
+	}
+
+	TM->Join();
 
 	ThreadManager::Delete();
 
